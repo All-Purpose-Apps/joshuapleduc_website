aa54c3e127c759d0429a99e40fa6faa21d8dcd2a
joshpled <joshuapleduc@gmail.com>
2024-09-12 17:33:57 -0400

    convert to link.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,12 @@
 import { Card, CardBody, CardFooter, CardTitle, Button, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 export default function ProjectCard({ title, subtitle, description, image, link, tech, concepts }) {
     return (
         <Col md="6">
             <Card className="card-profile card-plain">
                 <div className="card-avatar">
-                    <a href={link} target="_blank">
+                    <a href={link} target="_blank" rel="noopener noreferrer">
                         <img
                             alt="..."
                             src={image}
@@ -13,7 +14,7 @@ export default function ProjectCard({ title, subtitle, description, image, link,
                     </a>
                 </div>
                 <CardBody>
-                    <a href={link} target="_blank">
+                    <a href={link} target="_blank" rel="noopener noreferrer">
                         <div className="author">
                             <CardTitle tag="h4">{title}</CardTitle>
                             <h6 className="card-category text-muted">
@@ -26,30 +27,29 @@ export default function ProjectCard({ title, subtitle, description, image, link,
                     </p>
                     <p className="card-description text-center">
                         <strong>Tech:</strong> {tech}
-
                     </p>
                     <p className="card-description text-center">
                         <strong>Concepts:</strong> {concepts}
                     </p>
                 </CardBody>
                 <CardFooter className="text-center">
-                    <Button
-                        className="btn-link"
-                        color="twitter"
-                        href="/projects"
-                    >
-                        More Details
-                    </Button>
+                    {/* Use Link from react-router-dom */}
+                    <Link to="/projects">
+                        <Button className="btn-link" color="twitter">
+                            More Details
+                        </Button>
+                    </Link>
                     <Button
                         className="btn-link"
                         color="twitter"
                         href={link}
                         target="_blank"
+                        rel="noopener noreferrer"
                     >
                         Go to Deployed Project
                     </Button>
                 </CardFooter>
             </Card>
         </Col>
-    )
+    );
 }
